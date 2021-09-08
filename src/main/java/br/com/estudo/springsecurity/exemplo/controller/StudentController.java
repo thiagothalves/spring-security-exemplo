c4caa39f9bde801f22e6942f38ce67cc70ca84a0
Thiago Alves <thiagoalves.dsn.pack@alterdata.com.br>
2021-09-07 22:23:33 -0300

    rafactor controller

--- src/main/java/br/com/estudo/springsecurity/exemplo/controller/StudentController.java
+++ src/main/java/br/com/estudo/springsecurity/exemplo/controller/StudentController.java
@@ -1,29 +1,71 @@
 package br.com.estudo.springsecurity.exemplo.controller;
 
+import br.com.estudo.springsecurity.exemplo.domain.Student;
 import br.com.estudo.springsecurity.exemplo.dto.StudentDTO;
 import br.com.estudo.springsecurity.exemplo.service.StudentService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/student")
 public class StudentController {
 
-	@Autowired
-	StudentService studentService;
+    @Autowired
+    StudentService studentService;
 
-	@GetMapping(path = "{id}")
-	public ResponseEntity<StudentDTO> findById(@PathVariable("id") Long id) {
-		StudentDTO  find = studentService.findById(id);
-		if(find == null) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-		return ResponseEntity.ok().body(find);
-	}
+    @GetMapping
+    public ResponseEntity<Page<StudentDTO>> findAll(
+            @PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<StudentDTO> studentDTOS = studentService.findAll(pageable);
+        if (studentDTOS.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(studentDTOS, HttpStatus.OK);
+        }
+
+    }
+
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity<StudentDTO> findById(@PathVariable("id") Long id) {
+        StudentDTO find = studentService.findById(id);
+        if (find == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok().body(find);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<StudentDTO> create(@Valid @RequestBody StudentDTO studentDTO) {
+        Student student = new Student();
+        BeanUtils.copyProperties(studentDTO, student);
+        return new ResponseEntity<>(studentService.create(student), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<StudentDTO> update(@PathVariable(value = "id") Long id,
+                           @Valid @RequestBody StudentDTO studentDTO) {
+        Student student = new Student();
+        BeanUtils.copyProperties(studentDTO, student);
+        return ResponseEntity.ok().body(studentService.update(id, student));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> remove(@PathVariable(value = "id") Long id) {
+        studentService.remove(id);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
--- src/main/java/br/com/estudo/springsecurity/exemplo/controller/StudentManagementController.java
+++ src/main/java/br/com/estudo/springsecurity/exemplo/controller/StudentManagementController.java
@@ -1,10 +1,21 @@
 package br.com.estudo.springsecurity.exemplo.controller;
 
 import br.com.estudo.springsecurity.exemplo.domain.Student;
+import br.com.estudo.springsecurity.exemplo.dto.StudentDTO;
+import br.com.estudo.springsecurity.exemplo.service.StudentService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -12,40 +23,59 @@ import java.util.List;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class StudentManagementController {
 
-	private static List<Student> students;
+    @Autowired
+    StudentService studentService;
 
-	static {
-		students = List.of(new Student(1L, "Thiago Alves"), new Student(2L, "Heitor Corrêa"),
-				new Student(3L, "Sofia Corrêa"));
-	}
 
-	@GetMapping
-	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
-	public List<Student> findAll() {
-		return students;
-	}
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
+    public ResponseEntity<Page<StudentDTO>> findAll(
+            @PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 
-	@PostMapping
-	@PreAuthorize("hasAuthority('student:write')")
-	public void create(@RequestBody Student student) {
-		students.add(student);
-	}
+        Page<StudentDTO> studentDTOS = studentService.findAll(pageable);
+        if (studentDTOS.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(studentDTOS, HttpStatus.OK);
+        }
 
-	@DeleteMapping(path = "{id}")
-	@PreAuthorize("hasAuthority('student:write')")
-	public void remove(@PathVariable("id") Long id) {
-		students.removeIf(student -> student.getId().equals(id));
-	}
+    }
 
-	@PutMapping(path = "{id}")
-	@PreAuthorize("hasAuthority('student:write')")
-	public void update(@PathVariable("id") Long id, @RequestBody Student student) throws Exception {
 
-		students.forEach(e -> {
-			if (e.getId().equals(id)) {
-				e.setName(student.getName());
-			}
-		});
+    @GetMapping(path = "{id}")
+    @PreAuthorize("hasAuthority('student:write')")
+    public ResponseEntity<StudentDTO> findById(@PathVariable("id") Long id) {
+        StudentDTO find = studentService.findById(id);
+        if (find == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok().body(find);
+    }
 
-	}
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
+    public ResponseEntity<StudentDTO> create(@Valid @RequestBody StudentDTO studentDTO) {
+        Student student = new Student();
+        BeanUtils.copyProperties(studentDTO, student);
+        return new ResponseEntity<>(studentService.create(student), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasAuthority('student:write')")
+    public ResponseEntity<StudentDTO> update(@PathVariable(value = "id") Long id,
+                                             @Valid @RequestBody StudentDTO studentDTO) {
+        Student student = new Student();
+        BeanUtils.copyProperties(studentDTO, student);
+        return ResponseEntity.ok().body(studentService.update(id, student));
+    }
+
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasAuthority('student:write')")
+    public ResponseEntity<?> remove(@PathVariable(value = "id") Long id) {
+        studentService.remove(id);
+        return ResponseEntity.ok().build();
+    }
+
+
 }
